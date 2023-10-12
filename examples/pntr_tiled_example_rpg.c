c7800187fabc1cb4a0e879c6b12f49a543a069a5
David Konsumer <konsumer@jetboystudio.com>
2023-10-12 07:03:43 -0700

    tracking stuff

--- examples/pntr_tiled_example_rpg.c
+++ examples/pntr_tiled_example_rpg.c
@@ -7,15 +7,8 @@
 // just some lil utils to see what is in map
 #include "debug_map.h"
 
-typedef struct AppData {
-    struct nk_context* ctx;
-    cute_tiled_map_t* map;
-    int speed;
-    int x, y;
-} AppData;
-
 // I find it easier to use tile-objects to represent different objects/orientations (in "logic" layer)
-typedef enum LogicObjects {
+typedef enum LogicObject {
     LOGIC_OBJECTS_DOOR = 132,
     LOGIC_OBJECTS_ENEMY_BAT_E = 159,
     LOGIC_OBJECTS_ENEMY_BAT_N = 165,
@@ -66,6 +59,87 @@ typedef enum LogicObjects {
     LOGIC_OBJECTS_WELL = 162
 } LogicObject;
 
+typedef enum Direction {
+    DIRECTION_N,
+    DIRECTION_S,
+    DIRECTION_E,
+    DIRECTION_W
+} Direction;
+
+typedef enum EnemyType {
+    ENEMY_BAT,
+    ENEMY_BLOB,
+    ENEMY_GHOST,
+    ENEMY_SKELETON,
+    ENEMY_SPIDER
+} EnemyType;
+
+typedef struct Loot {
+    int x, y;
+    char* prize;
+} Loot;
+
+typedef struct Enemy {
+    int x, y;
+    EnemyType type;
+    Direction direction;
+} Enemy;
+
+typedef struct Animation {
+    int x, y;
+    LogicObject type;
+} Animation;
+
+typedef struct Trap {
+    int x, y;
+    LogicObject type;
+} Trap;
+
+typedef struct Switch {
+    int x, y;
+    bool engaged;
+    char* name;
+    LogicObject type;
+} Switch;
+
+typedef struct Sign {
+    int x, y;
+    char* text;
+} Sign;
+
+typedef struct Portal {
+    int x, y;
+    char* map;
+} Portal;
+
+typedef struct AppData {
+    struct nk_context* ctx;
+    cute_tiled_map_t* map;
+    int speed;
+    int x, y;
+    Direction player_facing;
+
+    Loot loots[32];
+    int loot_count;
+
+    Enemy enemies[32];
+    int enemy_count;
+
+    Animation animations[32];
+    int animation_count;
+
+    Trap traps[32];
+    int trap_count;
+
+    Switch switches[32];
+    int switch_count;
+
+    Sign signs[32];
+    int sign_count;
+
+    Portal portals[32];
+    int portal_count;
+} AppData;
 
 bool Init(pntr_app* app) {
     AppData* appData = pntr_load_memory(sizeof(AppData));
@@ -75,6 +149,14 @@ bool Init(pntr_app* app) {
     appData->x = 0;
     appData->y = 0;
     appData->speed = 200;
+
+    appData->loot_count = -1;
+    appData->enemy_count = -1;
+    appData->animation_count = -1;
+    appData->trap_count = -1;
+    appData->switch_count = -1;
+    appData->sign_count = -1;
+    appData->portal_count = -1;
 
     // print_map(appData->map);
 
@@ -112,39 +194,140 @@ bool Init(pntr_app* app) {
                 }
 
                 if (strcmp(o->type.ptr, "sign") == 0){
-                    // TODO: track sign tile/text/position
                     char* text;
                     for (int i=0; i < o->property_count; i++) {
                         cute_tiled_property_t* p = o->properties + i;
                         if (strcmp(p->name.ptr, "text") == 0 && p->type == CUTE_TILED_PROPERTY_STRING){
                             printf("found sign that says: %s\n", p->data.string.ptr);
+                            appData->signs[appData->sign_count++] = (Sign) { .x=posX, .y=posY, .text=p->data.string.ptr };
                         }
                     }
-
                 }
 
                 if (strcmp(o->type.ptr, "enemy") == 0){
-                    // TODO: track enemy direction/tileset/position
+                    Direction direction = DIRECTION_N;
+                    EnemyType enemy_type = ENEMY_BAT;
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_BAT_E) {
+                      direction=DIRECTION_E;
+                      enemy_type=ENEMY_BAT;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_BAT_N) {
+                      direction=DIRECTION_N;
+                      enemy_type=ENEMY_BAT;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_BAT_S) {
+                      direction=DIRECTION_S;
+                      enemy_type=ENEMY_BAT;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_BAT_W) {
+                      direction=DIRECTION_W;
+                      enemy_type=ENEMY_BAT;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_BLOB_E) {
+                      direction=DIRECTION_E;
+                      enemy_type=ENEMY_BLOB;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_BLOB_N) {
+                      direction=DIRECTION_N;
+                      enemy_type=ENEMY_BLOB;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_BLOB_S) {
+                      direction=DIRECTION_S;
+                      enemy_type=ENEMY_BLOB;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_BLOB_W) {
+                      direction=DIRECTION_W;
+                      enemy_type=ENEMY_BLOB;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_GHOST_E) {
+                      direction=DIRECTION_E;
+                      enemy_type=ENEMY_GHOST;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_GHOST_N) {
+                      direction=DIRECTION_N;
+                      enemy_type=ENEMY_GHOST;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_GHOST_S) {
+                      direction=DIRECTION_S;
+                      enemy_type=ENEMY_GHOST;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_GHOST_W) {
+                      direction=DIRECTION_W;
+                      enemy_type=ENEMY_GHOST;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_SKELETON_E) {
+                      direction=DIRECTION_E;
+                      enemy_type=ENEMY_SKELETON;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_SKELETON_N) {
+                      direction=DIRECTION_N;
+                      enemy_type=ENEMY_SKELETON;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_SKELETON_S) {
+                      direction=DIRECTION_S;
+                      enemy_type=ENEMY_SKELETON;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_SKELETON_W) {
+                      direction=DIRECTION_W;
+                      enemy_type=ENEMY_SKELETON;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_SPIDER_E) {
+                      direction=DIRECTION_E;
+                      enemy_type=ENEMY_SPIDER;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_SPIDER_N) {
+                      direction=DIRECTION_N;
+                      enemy_type=ENEMY_SPIDER;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_SPIDER_S) {
+                      direction=DIRECTION_S;
+                      enemy_type=ENEMY_SPIDER;
+                    }
+
+                    if (gid == LOGIC_OBJECTS_ENEMY_SPIDER_W) {
+                      direction=DIRECTION_W;
+                      enemy_type=ENEMY_SPIDER;
+                    }
+                    appData->enemies[appData->enemy_count++] = (Enemy) { .x=posX, .y=posY, .type=enemy_type, .direction=direction };
                 }
 
                 if (strcmp(o->type.ptr, "portal") == 0){
-                    // TODO: track portal tile/target/position
+                    appData->portals[appData->portal_count++] = (Portal) { .x=posX, .y=posY, .map=o->name.ptr };
                 }
 
                 if (strcmp(o->type.ptr, "trap") == 0){
-                    // TODO: track trap type/position
+                    appData->traps[appData->trap_count++] = (Trap) { .x=posX, .y=posY, .type=gid };
                 }
 
                 if (strcmp(o->type.ptr, "loot") == 0){
-                    // TODO: track loot prize/position
+                    appData->loots[appData->loot_count++] = (Loot) { .x=posX, .y=posY, .prize=o->name.ptr };
                 }
 
                 if (strcmp(o->type.ptr, "switch") == 0){
-                    // TODO: track switch color/trigger
+                    appData->switches[appData->switch_count++] = (Switch) { .x=posX, .y=posY, .engaged=false, .name=o->name.ptr, .type=gid };
                 }
 
                 if (strcmp(o->type.ptr, "animaton") == 0){
-                    // TODO: track animation type/position
+                    appData->animations[appData->animation_count++] = (Animation) { .x=posX, .y=posY, .type=gid };
                 }
 
                 o = o->next;
