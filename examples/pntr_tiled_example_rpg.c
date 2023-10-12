4f9abafdcd116838ef9f8caeb972d8dd295c1708
David Konsumer <konsumer@jetboystudio.com>
2023-10-12 15:02:42 -0700

    simplify map object rendering by combining iteration with drawing

--- examples/pntr_tiled_example_rpg.c
+++ examples/pntr_tiled_example_rpg.c
@@ -59,87 +59,89 @@ typedef enum LogicObject {
     LOGIC_OBJECTS_WELL = 162
 } LogicObject;
 
-typedef enum Direction {
-    DIRECTION_N,
-    DIRECTION_S,
-    DIRECTION_E,
-    DIRECTION_W
-} Direction;
-
-typedef enum EnemyType {
-    ENEMY_BAT,
-    ENEMY_BLOB,
-    ENEMY_GHOST,
-    ENEMY_SKELETON,
-    ENEMY_SPIDER
-} EnemyType;
-
-typedef struct Loot {
-    int x, y;
-    char* prize;
-} Loot;
-
-typedef struct Enemy {
-    int x, y;
-    EnemyType type;
-    Direction direction;
-} Enemy;
-
-typedef struct Animation {
-    int x, y;
-    LogicObject type;
-} Animation;
-
-typedef struct Trap {
-    int x, y;
-    LogicObject type;
-} Trap;
-
-typedef struct Switch {
-    int x, y;
-    bool engaged;
-    char* name;
-    LogicObject type;
-} Switch;
-
-typedef struct Sign {
-    int x, y;
-    char* text;
-} Sign;
-
-typedef struct Portal {
-    int x, y;
-    char* map;
-} Portal;
-
 typedef struct AppData {
     struct nk_context* ctx;
     cute_tiled_map_t* map;
     int speed;
     int x, y;
-    Direction player_facing;
-
-    Loot loots[32];
-    int loot_count;
-
-    Enemy enemies[32];
-    int enemy_count;
-
-    Animation animations[32];
-    int animation_count;
-
-    Trap traps[32];
-    int trap_count;
-
-    Switch switches[32];
-    int switch_count;
-
-    Sign signs[32];
-    int sign_count;
-
-    Portal portals[32];
-    int portal_count;
+    bool sign_open;
+    char* sign_text;
 } AppData;
+
+
+// this will update/draw all the map-objects, including player, based on state of things
+void update_map_objects(AppData* appData) {
+    cute_tiled_layer_t* layer = appData->map->layers;
+
+    while(layer) {
+        printf("LAYER: %s\n", layer->name.ptr);
+
+        // this is the layer where I keep collision-shapes (to block where the player/enemies can go)
+        // this could also be done with proper Tiled collision-stuff, this is just how I set it up
+        if (strcmp(layer->name.ptr, "collision") == 0) {
+            // TODO: check collisions
+        }
+
+        // this is the layer where I keep object-pointers that determine game-logic
+        if (strcmp(layer->name.ptr, "logic") == 0) {
+            cute_tiled_object_t* o = layer->objects;
+            while (o) {
+                int gid = o->gid;
+
+                // these are the tile-space coordinates
+                int tileX = (int) (o->x/16.0);
+                int tileY = (int) (o->y/16.0);
+
+                printf("  OBJECT (%dx%d) - %d: %s (%s)\n", tileX, tileY, gid, o->name.ptr, o->type.ptr);
+
+                if (strcmp(o->type.ptr, "sign") == 0){
+                    char* text;
+                    for (int i=0; i < o->property_count; i++) {
+                        cute_tiled_property_t* p = o->properties + i;
+                        if (strcmp(p->name.ptr, "text") == 0 && p->type == CUTE_TILED_PROPERTY_STRING){
+                            printf("%s\n---\n", p->data.string.ptr);
+                            // TODO: draw sign, or show text
+                        }
+
+                        if (strcmp(o->type.ptr, "enemy") == 0){
+                            // TODO: you can figure out the enemy tiles form the gid.
+                            // it's not a direct usage, but it's a marker (see LogicObject) that indicates whcih enemy/direction
+                        }
+                    }
+                }
+
+                if (strcmp(o->type.ptr, "player") == 0){
+                    // TODO: update position/direction, use gid to work out correct direction tiles
+                }
+
+                if (strcmp(o->type.ptr, "portal") == 0){
+                    // TODO: you can figure out the portal tiles from the gid.
+                }
+
+                if (strcmp(o->type.ptr, "trap") == 0){
+                    // TODO: you can figure out the trap tiles from the gid.
+                }
+
+                if (strcmp(o->type.ptr, "loot") == 0){
+                    // TODO: you can figure out the loot prize from the name.
+                }
+
+                if (strcmp(o->type.ptr, "switch") == 0){
+                    // TODO: you can figure out the switch tiles from the gid, the variable it toggles is the name.
+                }
+
+                if (strcmp(o->type.ptr, "animaton") == 0){
+                    // TODO: you can figure out the animation tiles from the gid.
+                }
+
+
+                o = o->next;
+            }
+        }
+        layer = layer->next;
+    }
+}
+
 
 bool Init(pntr_app* app) {
     AppData* appData = pntr_load_memory(sizeof(AppData));
@@ -150,191 +152,10 @@ bool Init(pntr_app* app) {
     appData->y = 0;
     appData->speed = 200;
 
-    appData->loot_count = -1;
-    appData->enemy_count = -1;
-    appData->animation_count = -1;
-    appData->trap_count = -1;
-    appData->switch_count = -1;
-    appData->sign_count = -1;
-    appData->portal_count = -1;
+    appData->sign_open = false;
+    appData->sign_text = "";
 
     // print_map(appData->map);
-
-    // find the objects and set them up
-    cute_tiled_layer_t* layer = appData->map->layers;
-    while(layer) {
-        printf("LAYER: %s\n", layer->name.ptr);
-
-        // this is the layer where I keep collision-shapes (to block where the player can go)
-        // this could also be done with proper tiled collisions, this is just how I set it up.
-        if (strcmp(layer->name.ptr, "collision") == 0) {
-            // TODO: store collision shapes
-        }
-
-        // this is the layer where I keep object-pointers that determine game-logic
-        if (strcmp(layer->name.ptr, "logic") == 0) {
-            cute_tiled_object_t* o = layer->objects;
-            while (o) {
-                // this is the tile position
-                int posX = (int) (o->x/16.0);
-                int posY = (int) (o->y/16.0);
-
-                // for tile-objects, this will tell you the tile (for direction/style)
-                // you could actually just use this to determine all the types, and not even set classes/types in editor
-                int gid = o->gid;
-
-                printf("  OBJECT (%dx%d) - %d: %s (%s)\n", posX, posY, gid, o->name.ptr, o->type.ptr);
-
-                if (strcmp(o->type.ptr, "player") == 0){
-                    // TODO: this does not change the view of the map
-                    // set position to player
-                    appData->x = (int) o->x;
-                    appData->y = (int) o->y;
-                    // TODO: track player direction (based on gid)
-                }
-
-                if (strcmp(o->type.ptr, "sign") == 0){
-                    char* text;
-                    for (int i=0; i < o->property_count; i++) {
-                        cute_tiled_property_t* p = o->properties + i;
-                        if (strcmp(p->name.ptr, "text") == 0 && p->type == CUTE_TILED_PROPERTY_STRING){
-                            printf("found sign that says: %s\n", p->data.string.ptr);
-                            appData->signs[appData->sign_count++] = (Sign) { .x=posX, .y=posY, .text=p->data.string.ptr };
-                        }
-                    }
-                }
-
-                if (strcmp(o->type.ptr, "enemy") == 0){
-                    Direction direction = DIRECTION_N;
-                    EnemyType enemy_type = ENEMY_BAT;
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_BAT_E) {
-                      direction=DIRECTION_E;
-                      enemy_type=ENEMY_BAT;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_BAT_N) {
-                      direction=DIRECTION_N;
-                      enemy_type=ENEMY_BAT;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_BAT_S) {
-                      direction=DIRECTION_S;
-                      enemy_type=ENEMY_BAT;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_BAT_W) {
-                      direction=DIRECTION_W;
-                      enemy_type=ENEMY_BAT;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_BLOB_E) {
-                      direction=DIRECTION_E;
-                      enemy_type=ENEMY_BLOB;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_BLOB_N) {
-                      direction=DIRECTION_N;
-                      enemy_type=ENEMY_BLOB;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_BLOB_S) {
-                      direction=DIRECTION_S;
-                      enemy_type=ENEMY_BLOB;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_BLOB_W) {
-                      direction=DIRECTION_W;
-                      enemy_type=ENEMY_BLOB;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_GHOST_E) {
-                      direction=DIRECTION_E;
-                      enemy_type=ENEMY_GHOST;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_GHOST_N) {
-                      direction=DIRECTION_N;
-                      enemy_type=ENEMY_GHOST;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_GHOST_S) {
-                      direction=DIRECTION_S;
-                      enemy_type=ENEMY_GHOST;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_GHOST_W) {
-                      direction=DIRECTION_W;
-                      enemy_type=ENEMY_GHOST;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_SKELETON_E) {
-                      direction=DIRECTION_E;
-                      enemy_type=ENEMY_SKELETON;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_SKELETON_N) {
-                      direction=DIRECTION_N;
-                      enemy_type=ENEMY_SKELETON;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_SKELETON_S) {
-                      direction=DIRECTION_S;
-                      enemy_type=ENEMY_SKELETON;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_SKELETON_W) {
-                      direction=DIRECTION_W;
-                      enemy_type=ENEMY_SKELETON;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_SPIDER_E) {
-                      direction=DIRECTION_E;
-                      enemy_type=ENEMY_SPIDER;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_SPIDER_N) {
-                      direction=DIRECTION_N;
-                      enemy_type=ENEMY_SPIDER;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_SPIDER_S) {
-                      direction=DIRECTION_S;
-                      enemy_type=ENEMY_SPIDER;
-                    }
-
-                    if (gid == LOGIC_OBJECTS_ENEMY_SPIDER_W) {
-                      direction=DIRECTION_W;
-                      enemy_type=ENEMY_SPIDER;
-                    }
-                    appData->enemies[appData->enemy_count++] = (Enemy) { .x=posX, .y=posY, .type=enemy_type, .direction=direction };
-                }
-
-                if (strcmp(o->type.ptr, "portal") == 0){
-                    appData->portals[appData->portal_count++] = (Portal) { .x=posX, .y=posY, .map=o->name.ptr };
-                }
-
-                if (strcmp(o->type.ptr, "trap") == 0){
-                    appData->traps[appData->trap_count++] = (Trap) { .x=posX, .y=posY, .type=gid };
-                }
-
-                if (strcmp(o->type.ptr, "loot") == 0){
-                    appData->loots[appData->loot_count++] = (Loot) { .x=posX, .y=posY, .prize=o->name.ptr };
-                }
-
-                if (strcmp(o->type.ptr, "switch") == 0){
-                    appData->switches[appData->switch_count++] = (Switch) { .x=posX, .y=posY, .engaged=false, .name=o->name.ptr, .type=gid };
-                }
-
-                if (strcmp(o->type.ptr, "animaton") == 0){
-                    appData->animations[appData->animation_count++] = (Animation) { .x=posX, .y=posY, .type=gid };
-                }
-
-                o = o->next;
-            }
-        }
-        layer = layer->next;
-    }
 
     return true;
 }
@@ -382,8 +203,8 @@ bool Update(pntr_app* app, pntr_image* screen) {
     // Draw the map
     pntr_draw_tiled(screen, appData->map, appData->x, appData->y, PNTR_WHITE);
 
-    // TODO: draw sprites of objects
-    // TODO: handle enemies & collisions
+    // update all map objects, and draw them
+    update_map_objects(appData);
 
     return true;
 }
