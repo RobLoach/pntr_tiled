f1aecad6e09a6e67acab89de0e4b570e560c5ba8
David Konsumer <konsumer@jetboystudio.com>
2023-10-14 18:04:02 -0700

    stripping all the mapping stuff I had to do before

--- examples/pntr_tiled_example_rpg.c
+++ examples/pntr_tiled_example_rpg.c
@@ -7,74 +7,6 @@
 // just some lil utils to see what is in map
 #include "debug_map.h"
 
-// Even though these are in map, they are not drawn.
-// I find it easier to use tile-objects to represent
-// different objects/orientations. So, I setup a tileset
-// that is just the stuff, then put it on "logic" layer.
-// This can also be done with any object, with properties,
-// if you need something more advanced, but you can encode
-// a lot of the same info in type/name/sprite-id, and I
-// like that I can see it in map-editor, more quickly.
-typedef enum LogicObject {
-    LOGIC_PLAYER_E = 133,
-    LOGIC_PLAYER_N = 139,
-    LOGIC_PLAYER_S = 121,
-    LOGIC_PLAYER_W = 127,
-
-    LOGIC_LOOT = 161,
-    LOGIC_POT = 125,
-    LOGIC_SIGN = 168,
-
-    LOGIC_TORCH_BLUE = 137,
-    LOGIC_TORCH_GREEN = 149,
-    LOGIC_TORCH_ORANGE = 155,
-    LOGIC_TORCH_RED = 143,
-    LOGIC_TORCH_YELLOW = 131,
-
-    LOGIC_ANIM_FIREPLACE = 167,
-    LOGIC_ANIM_WATER = 148,
-
-    LOGIC_ENEMY_BAT_E = 159,
-    LOGIC_ENEMY_BAT_N = 165,
-    LOGIC_ENEMY_BAT_S = 147,
-    LOGIC_ENEMY_BAT_W = 153,
-    LOGIC_ENEMY_BLOB_E = 135,
-    LOGIC_ENEMY_BLOB_N = 141,
-    LOGIC_ENEMY_BLOB_S = 123,
-    LOGIC_ENEMY_BLOB_W = 129,
-    LOGIC_ENEMY_GHOST_E = 136,
-    LOGIC_ENEMY_GHOST_N = 142,
-    LOGIC_ENEMY_GHOST_S = 124,
-    LOGIC_ENEMY_GHOST_W = 130,
-    LOGIC_ENEMY_SKELETON_E = 134,
-    LOGIC_ENEMY_SKELETON_N = 140,
-    LOGIC_ENEMY_SKELETON_S = 122,
-    LOGIC_ENEMY_SKELETON_W = 128,
-    LOGIC_ENEMY_SPIDER_E = 158,
-    LOGIC_ENEMY_SPIDER_N = 164,
-    LOGIC_ENEMY_SPIDER_S = 146,
-    LOGIC_ENEMY_SPIDER_W = 152,
-
-    LOGIC_PORTAL_DOOR = 132,
-    LOGIC_PORTAL_GATE = 126,
-    LOGIC_PORTAL_STAIRS_DOWN_E = 150,
-    LOGIC_PORTAL_STAIRS_DOWN_W = 138,
-    LOGIC_PORTAL_STAIRS_UP_E = 144,
-    LOGIC_PORTAL_STAIRS_UP_W = 156,
-    LOGIC_PORTAL_WELL = 162,
-
-    LOGIC_SWITCH_BLUE = 151,
-    LOGIC_SWITCH_GREEN = 163,
-    LOGIC_SWITCH_ORANGE = 157,
-    LOGIC_SWITCH_RED = 145,
-
-    LOGIC_TRAP_FIRE = 166,
-    LOGIC_TRAP_LAVA = 154,
-    LOGIC_TRAP_SPIKES = 160
-} LogicObject;
-
-
-
 typedef struct AppData {
     struct nk_context* ctx;
     cute_tiled_map_t* map;
@@ -89,17 +21,12 @@ typedef struct AppData {
     // set to NULL or current text that should show on screen
     char* sign_text;
     
-    // these are layers that are drawn over map by update_map_objects
-    pntr_image* layer_player;
-    pntr_image* layer_enemies;
-    pntr_image* layer_things;
+    // layer that is drawn over map by update_map_objects
+    pntr_image* objects;
 
-    // these are spritesheets that will be used to draw objects
-    // generally it;s a good idea to put them all togther on one
-    // but this is how I had the map setup
-    pntr_image* sprites_basictiles;
-    pntr_image* sprites_characters;
-    pntr_image* sprites_things;
+    // this will be used to draw sprites
+    // it's the same spritesheet in the map
+    pntr_image* sprites;
 } AppData;
 
 // unload current map and switch to another one
@@ -163,78 +90,15 @@ void update_map_objects(AppData* appData) {
 
                 if (strcmp(o->type.ptr, "player") == 0){
                     // TODO: do animation and update position
-                    if (gid == LOGIC_PLAYER_N) {
-                        // 39, 40, 41
-                        pntr_draw_image_rec(appData->layer_player, appData->sprites_characters, get_tile_rec(40, appData->sprites_characters), posX, posY);
-                    } else if (gid == LOGIC_PLAYER_S) {
-                        // 3, 4, 5
-                        pntr_draw_image_rec(appData->layer_player, appData->sprites_characters, get_tile_rec(4, appData->sprites_characters), posX, posY);
-                    } if (gid == LOGIC_PLAYER_E) {
-                        // 27, 28, 29
-                        pntr_draw_image_rec(appData->layer_player, appData->sprites_characters, get_tile_rec(28, appData->sprites_characters), posX, posY);
-                    } if (gid == LOGIC_PLAYER_W) {
-                        // 15, 16, 17
-                        pntr_draw_image_rec(appData->layer_player, appData->sprites_characters, get_tile_rec(16, appData->sprites_characters), posX, posY);
-                    }
+                    pntr_draw_image_rec(appData->objects, appData->sprites, get_tile_rec(gid, appData->sprites), posX, posY);
                 }
 
                 else if (strcmp(o->type.ptr, "enemy") == 0){
-                    int tile_id = 0;
-
-                    if (gid == LOGIC_ENEMY_BAT_E || gid == LOGIC_ENEMY_BAT_N || gid == LOGIC_ENEMY_BAT_S || gid == LOGIC_ENEMY_BAT_W) {
-
-                    }
-                    else if (gid == LOGIC_ENEMY_BLOB_E || gid == LOGIC_ENEMY_BLOB_N || gid == LOGIC_ENEMY_BLOB_S || gid == LOGIC_ENEMY_BLOB_W) {
-
-                    }
-                    else if (gid == LOGIC_ENEMY_GHOST_E || gid == LOGIC_ENEMY_GHOST_N || gid == LOGIC_ENEMY_GHOST_S || gid == LOGIC_ENEMY_GHOST_W) {
-
-                    }
-                    else if (gid == LOGIC_ENEMY_SKELETON_E || gid == LOGIC_ENEMY_SKELETON_N || gid == LOGIC_ENEMY_SKELETON_S || gid == LOGIC_ENEMY_SKELETON_W) {
-
-                    }
-                    else if (gid == LOGIC_ENEMY_SPIDER_E || gid == LOGIC_ENEMY_SPIDER_N || gid == LOGIC_ENEMY_SPIDER_S || gid == LOGIC_ENEMY_SPIDER_W) {
-
-                    }
-
-                    if (tile_id) {
-                        pntr_draw_image_rec(appData->layer_player, appData->sprites_basictiles, get_tile_rec(tile_id, appData->sprites_basictiles), posX, posY);
-                        // TODO: check for near & action button
-                    }
-
+                    // TODO you can figure out the enemy type/direction from the gid.
                 }
 
                 else if (strcmp(o->type.ptr, "portal") == 0){
-                    int tile_id = 0;
-
-                    if (gid == LOGIC_PORTAL_DOOR) {
-                        // TODO: this is animated on activation
-                        tile_id = 48;
-                    }
-                    else if (gid == LOGIC_PORTAL_GATE) {
-                        // TODO: this is animated on activation
-                        tile_id = 49;
-                    }
-                    else if (gid == LOGIC_PORTAL_STAIRS_DOWN_E) {
-                        tile_id = 105;
-                    }
-                    else if (gid == LOGIC_PORTAL_STAIRS_DOWN_W) {
-                        tile_id = 104;
-                    }
-                    else if (gid == LOGIC_PORTAL_STAIRS_UP_E) {
-                        tile_id = 113;
-                    }
-                    else if (gid == LOGIC_PORTAL_STAIRS_UP_W) {
-                        tile_id = 112;
-                    }
-                    else if (gid == LOGIC_PORTAL_WELL) {
-                        tile_id = 31;
-                    }
-
-                    if (tile_id) {
-                        pntr_draw_image_rec(appData->layer_player, appData->sprites_basictiles, get_tile_rec(tile_id, appData->sprites_basictiles), posX, posY);
-                        // TODO: check for near & action button
-                    }
+                    // TODO you can figure out the tile from the gid, the desitnation from the name.
                 }
 
                 else if (strcmp(o->type.ptr, "trap") == 0){
@@ -254,15 +118,15 @@ void update_map_objects(AppData* appData) {
                 }
 
                 else if (strcmp(o->type.ptr, "sign") == 0){
-                    char* text;
-                    for (int i=0; i < o->property_count; i++) {
-                        cute_tiled_property_t* p = o->properties + i;
-                        if (strcmp(p->name.ptr, "text") == 0 && p->type == CUTE_TILED_PROPERTY_STRING){
-                            printf("%s\n---\n", p->data.string.ptr);
-                            pntr_draw_image_rec(appData->layer_things, appData->sprites_basictiles, get_tile_rec(67, appData->sprites_basictiles), posX, posY);
-                            // TODO: & handle trigger & show text
-                        }
-                    }
+                    // char* text;
+                    // for (int i=0; i < o->property_count; i++) {
+                    //     cute_tiled_property_t* p = o->properties + i;
+                    //     if (strcmp(p->name.ptr, "text") == 0 && p->type == CUTE_TILED_PROPERTY_STRING){
+                    //         printf("%s\n---\n", p->data.string.ptr);
+                    //         pntr_draw_image_rec(appData->layer_things, appData->sprites_basictiles, get_tile_rec(67, appData->sprites_basictiles), posX, posY);
+                    //         // TODO: & handle trigger & show text
+                    //     }
+                    // }
                 }
 
 
@@ -290,16 +154,12 @@ bool Init(pntr_app* app) {
     int w = appData->map->width * appData->map->tilewidth;
     int h = appData->map->height * appData->map->tileheight;
 
-    // create layers for objects to be drawn on
-    appData->layer_player = pntr_new_image(w, h);
-    appData->layer_enemies = pntr_new_image(w, h);
-    appData->layer_things = pntr_new_image(w, h);
+    // create layer for objects to be drawn on
+    appData->objects = pntr_new_image(w, h);
 
     // setup all the spritesheets for objects
     // TODO: are these already loaded/quaded by map?
-    appData->sprites_basictiles = pntr_load_image("resources/rpg/basictiles.png");
-    appData->sprites_characters = pntr_load_image("resources/rpg/characters.png");
-    appData->sprites_things = pntr_load_image("resources/rpg/things.png");
+    appData->sprites = pntr_load_image("resources/rpg/sprites.png");
 
     return true;
 }
@@ -337,9 +197,7 @@ bool Update(pntr_app* app, pntr_image* screen) {
 
     // update all map objects, and draw them
     update_map_objects(appData);
-    pntr_draw_image(screen, appData->layer_things, appData->x, appData->y);
-    pntr_draw_image(screen, appData->layer_enemies, appData->x, appData->y);
-    pntr_draw_image(screen, appData->layer_player, appData->x, appData->y);
+    pntr_draw_image(screen, appData->objects, appData->x, appData->y);
 
     return true;
 }
