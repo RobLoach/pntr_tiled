474dc2149fbed34c8eef0422444660332903c4f2
Rob Loach <robloach@gmail.com>
2025-03-03 11:08:30 -0500

    Update

--- cmake/Findpntr_assetsys.cmake
+++ cmake/Findpntr_assetsys.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
     pntr_assetsys
     GIT_REPOSITORY https://github.com/robloach/pntr_assetsys.git
-    GIT_TAG bf60431
+    GIT_TAG 4b1c8c5
 )
 FetchContent_MakeAvailable(pntr_assetsys)
